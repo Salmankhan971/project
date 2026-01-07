0c4088f73b979358aaef81063b649f8f349c92e0
Salmankhan971 <saadsdam55@gmail.com>
2026-01-07 20:32:26 +0500

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,22 @@
+function login() {
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+
+    if (user === "admin" && pass === "1234") {
+        localStorage.setItem("loggedIn", "true");
+        window.location.href = "index.html";
+    } else {
+        document.getElementById("error").innerText = "Invalid Login";
+    }
+}
+
+function checkLogin() {
+    if (localStorage.getItem("loggedIn") !== "true") {
+        window.location.href = "login.html";
+    }
+}
+
+function logout() {
+    localStorage.removeItem("loggedIn");
+    window.location.href = "login.html";
+}
